8101777e89330b0ac28826a23b912cb295649347
ei17ringo <ei17ringo@gmail.com>
2014-12-03 16:33:27 +0800

    Initial Commit

---
+++ SampleNotificationOfDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleNotificationOfDate
+//
+//  Created by Eriko Ichinohe on 2014/12/03.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleNotificationOfDate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleNotificationOfDate
+//
+//  Created by Eriko Ichinohe on 2014/12/03.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
